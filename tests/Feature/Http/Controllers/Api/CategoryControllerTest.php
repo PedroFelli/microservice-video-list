c5f8465eb4e3cd4b7d25c13285b365bf8b169d3b
Pedro Fellipe <pedrofellipeunai@gmail.com>
2021-04-09 08:26:31 -0300

    test category controller

--- tests/Feature/Http/Controllers/Api/CategoryControllerTest.php
+++ tests/Feature/Http/Controllers/Api/CategoryControllerTest.php
@@ -5,11 +5,13 @@ namespace Tests\Feature\Http\Controllers\Api;
 use App\Models\Category;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
+use Illuminate\Foundation\Testing\TestResponse;
 use Tests\TestCase;
 
 class CategoryControllerTest extends TestCase
 {
     use DatabaseMigrations;
+
     /**
      * A basic feature test example.
      *
@@ -28,21 +30,92 @@ class CategoryControllerTest extends TestCase
     public function testShow()
     {
         $category = factory(Category::class)->create();
-        $response = $this->get(route('categories.show',['category' => $category->id]));
+        $response = $this->get(route('categories.show', ['category' => $category->id]));
 
         $response
             ->assertStatus(200)
             ->assertJson($category->toArray());
     }
 
-    public function testInvalidationData(){
-        $response = $this->json('POST',route('categories.store'), []);
+    public function testInvalidationData()
+    {
+        $response = $this->json('POST', route('categories.store'), []);
+
+        $this->assertInvalidationRequired($response);
+        $response = $this->json('POST', route('categories.store'), [
+            'name' => str_repeat('a', 256),
+            'is_active' => 'a'
+        ]);
+        $this->assertInvalidationMax($response);
+        $this->assertInvalidationBoolean($response);
+
+
+        $category = factory(Category::class)->create();
+        $response = $this->json('PUT', route('categories.update', ['category' => $category->id]), []);
+        $this->assertInvalidationRequired($response);
+
+        $response = $this->json('PUT', route('categories.update', ['category' => $category->id]),
+            [
+                'name' => str_repeat('a', 256),
+                'is_active' => 'a'
+            ]
+        );
+        $this->assertInvalidationMax($response);
+        $this->assertInvalidationBoolean($response);
+    }
+
+    protected function assertInvalidationRequired(TestResponse $response){
+        $response
+            ->assertStatus(422)
+            ->assertJsonValidationErrors(['name'])
+            ->assertJsonMissingValidationErrors(['is_active'])
+            ->assertJsonFragment([
+                \Lang::get('validation.required', ['attribute' => 'name'])
+            ]);
+
+    }
+    protected function assertInvalidationMax(TestResponse $response){
         $response
             ->assertStatus(422)
             ->assertJsonValidationErrors(['name'])
             ->assertJsonFragment([
-                \Lang::get('validation.required', [ 'attribute' => 'name'])
+                \Lang::get('validation.max.string', ['attribute' => 'name', 'max' => 255])
             ]);
+    }
+    protected function assertInvalidationBoolean(TestResponse $response){
+        $response
+            ->assertStatus(422)
+            ->assertJsonValidationErrors(['is_active'])
+            ->assertJsonFragment([
+                \Lang::get('validation.boolean', ['attribute' => 'is active'])
+            ]);
+
+    }
+
+    public function testStore(){
+        $response = $this->json('POST', route('categories.store'), [
+            'name' => 'test',
+        ]);
+
+        $id = $response->json('id');
+        $category = Category::find($id);
+
+        $response
+            ->assertStatus(201)
+            ->assertJson($category->toArray());
+        $this->assertTrue($response->json('is_active'));
+        $this->assertNull($response->json('description'));
+
+        $response = $this->json('POST', route('categories.store'), [
+            'name' => 'test',
+            'description'=> 'description',
+            'is_active' => false
+        ]);
+
+        $response->assertJsonFragment([
+            'description' => 'description',
+            'is_active' => false
+        ]);
 
     }
 }
